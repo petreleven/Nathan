fb61b18a932a7166b9d01b80d719ffe2663c58b8
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-06-25 17:56:26 +0300

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+"""
+data           |   type        
+---------------------------------
+words,sentences|  strings(str)  
+whole numbers  |  integers(int)
+decimal numbers|  floats
+True and False |  booleans (bool)
+"""
+myname = 29.626
+print ( type(myname) )
+
+is_game_running = False
+print(type(is_game_running))
